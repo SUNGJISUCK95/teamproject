7d6fb1014023467355eb0b9d17b4754b2a2a9c0c
dongju1311 <ghkd1311@naver.com>
2025-10-29 15:57:51 +0900

    2025-10-29 제품 상세 페이지: 황동주

--- bicycle-app/frontend/src/components/commons/Header.jsx
+++ bicycle-app/frontend/src/components/commons/Header.jsx
@@ -4,80 +4,74 @@ import { FaHeadset, FaUser, FaBars, FaTimes } from "react-icons/fa";
 import '../../styles/purchaseheader.css';
 
 export function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [purchaseMenuOpen,setPurchaseMenuOpen] = useState(null);
-  const location = useLocation();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [purchaseMenuOpen,setPurchaseMenuOpen] = useState(null);
+    const location = useLocation();
 
-  return (
-    <header className="header" onMouseLeave={()=>setPurchaseMenuOpen(null)}>
-      {/* 좌측 로고 */}
-      <div className="header-left">
-        <Link to="/" className="logo">
-          Bicycle-App
-        </Link>
-      </div>
-
-      {/* 중앙 메뉴 (데스크톱용) */}
-      <nav className={`header-center ${menuOpen ? "open" : ""}`}>
-        <ul>
-          <li onMouseEnter={() => setPurchaseMenuOpen('purchase')}>
-            <NavLink to="/product" onClick={() => setMenuOpen(false)}
-                     // className={() => {
-                     //     const isProductPage = location.pathname.startsWith('/product') ||
-                     //         location.pathname.startsWith('/products');
-                     //     return isProductPage ? 'active' : '';
-                     // }}
-                >
-              자전거 구매
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/rental" onClick={() => setMenuOpen(false)}>
-              자전거 대여
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/travel" onClick={() => setMenuOpen(false)}>
-              여행지 추천
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-
-      {/* 우측 메뉴 */}
-      <div className="header-right">
-        <NavLink to="/support" className="icon-link">
-          <FaHeadset className="icon" />
-          <span className="text">고객센터</span>
-        </NavLink>
-        <NavLink to="/login" className="icon-link">
-          <FaUser className="icon" />
-          <span className="text">로그인</span>
-        </NavLink>
-        <button className="search-btn">
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </button>
-
-        {/* 햄버거 버튼 (모바일용) */}
-        <button
-          className="menu-btn"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="메뉴 열기"
-        >
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </button>
-      </div>
-        {purchaseMenuOpen === 'purchase' && (
-            <div className="submenu-container">
-                <div className="submenu-content">
-                    <Link to="/products/mountain">산악</Link>
-                    <Link to="/products/road">로드</Link>
-                    <Link to="/products/lifestyle">라이프스타일</Link>
-                    <Link to="/products/electric">전기</Link>
-                    <Link to="/products/brand">브랜드</Link>
-                </div>
+    return (
+        <header className="header" onMouseLeave={()=>setPurchaseMenuOpen(null)}>
+            {/* 좌측 로고 */}
+            <div className="header-left">
+                <Link to="/" className="logo">
+                    Bicycle-App
+                </Link>
             </div>
-        )}
-    </header>
-  );
+
+            {/* 중앙 메뉴 (데스크톱용) */}
+            <nav className={`header-center ${menuOpen ? "open" : ""}`}>
+                <ul>
+                    <li onMouseEnter={() => setPurchaseMenuOpen('purchase')}>
+                        <NavLink to="/products/mountain" onClick={() => setMenuOpen(false)}>
+                            자전거 구매
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/rental" onClick={() => setMenuOpen(false)}>
+                            자전거 대여
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/travel" onClick={() => setMenuOpen(false)}>
+                            여행지 추천
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+            {/* 우측 메뉴 */}
+            <div className="header-right">
+                <NavLink to="/support" className="icon-link">
+                    <FaHeadset className="icon" />
+                    <span className="text">고객센터</span>
+                </NavLink>
+                <NavLink to="/login" className="icon-link">
+                    <FaUser className="icon" />
+                    <span className="text">로그인</span>
+                </NavLink>
+                <button className="search-btn">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+
+                {/* 햄버거 버튼 (모바일용) */}
+                <button
+                    className="menu-btn"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    aria-label="메뉴 열기"
+                >
+                    {menuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+            </div>
+            {purchaseMenuOpen === 'purchase' && (
+                <div className="submenu-container">
+                    <div className="submenu-content">
+                        <Link to="/products/mountain">산악</Link>
+                        <Link to="/products/road">로드</Link>
+                        <Link to="/products/lifestyle">라이프스타일</Link>
+                        <Link to="/products/electric">전기</Link>
+                        {/*<Link to="/products/brand">브랜드</Link>*/}
+                    </div>
+                </div>
+            )}
+        </header>
+    );
 }
