67a24f33b5d32fa9be9a37c833b1429bdc8e4872
kang <kijong4309@naver.com>
2025-11-17 18:06:37 +0900

    강기종 - 헤더, 푸터, 챗봇 수정

--- bicycle-app/frontend/src/components/commons/Footer.jsx
+++ bicycle-app/frontend/src/components/commons/Footer.jsx
@@ -61,9 +61,15 @@ export function Footer() {
         {/* 오른쪽 회사 정보 */}
         <div className="footer-info">
           <div className="footer-social">
-            <a href="https://www.instagram.com/cellobike_official/"><i className="fab fa-instagram"></i></a>
-            <a href="https://www.youtube.com/channel/UCgb2432J7dUqZXaWe39FqQw"><i className="fab fa-youtube"></i></a>
-            <a href="https://blog.naver.com/celloblog"><i className="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/cellobike_official/" target="_blank">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UCgb2432J7dUqZXaWe39FqQw" target="_blank">
+              <i className="fab fa-youtube"></i>
+            </a>
+            <a href="https://blog.naver.com/celloblog" target="_blank">
+              <i className="fa-solid fa-blog"></i>
+            </a>
           </div>
           <p>
             대표 홍길동 | 사업자등록번호 123-45-67890 <br />
