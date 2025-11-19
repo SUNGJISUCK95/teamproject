5e2c4d97b709a66af761c929faa63dacf2576c48
dongju1311 <ghkd1311@naver.com>
2025-11-19 11:27:46 +0900

    2025-11-19 수정중

--- bicycle-app/frontend/src/pages/Cart.jsx
+++ bicycle-app/frontend/src/pages/Cart.jsx
@@ -1,11 +1,11 @@
-import {CartHeader} from "../components/cartItem/CartHeader.jsx";
-import {CartShippingInfo} from "../components/cartItem/CartShippingInfo.jsx"
-import '../styles/cartItem/cartItem.css';
-import {CartItem} from "../components/cartItem/CartItem.jsx";
+import {CartHeader} from "../components/cart/CartHeader.jsx";
+import {CartShippingInfo} from "../components/cart/CartShippingInfo.jsx"
+import {CartItem} from "../components/cart/CartItem.jsx";
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
-import {showCart} from "../feature/cartItem/cartAPI.js";
+import {showCart} from "../feature/cart/cartAPI.js";
 import {useNavigate} from "react-router-dom";
+import '../styles/cart/cart.css';
 
 export function Cart(){
     const dispatch = useDispatch();
